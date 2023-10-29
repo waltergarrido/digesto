314a7469c41f12b113a1ad6826ae53242e8ab67b
unknown <blackwalter7@gmail.com>
2023-10-29 17:57:15 -0300

    (form-carga): pantalla de ingreso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { DragAndDropComponent } from './drag-and-drop/drag-and-drop.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form-carga/form-carga.component.ts
+++ src/app/form-carga/form-carga.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-carga.component.css']
 })
 export class FormCargaComponent {
+   public tipos= [];
+   public seleccion:any ='seleccionar' ;
+  DigestoCategoria=[
+    {nombre:'Actividad de los Administrados',
+    tipos:['Actividades Diversas'
+      ,'Comercio'
+      ,'Comercio De Productos Alimenticios'
+      ,'Espectáculos'
+      ,'Habilitaciones E Inspecciones'
+      ,'Industria'
+      ,'Industria Y Comercio De Productos Alimenticios'
+      ,'Moralidad'
+      ,'Sanidad Y Educación'
+     ]},
+     {nombre:'Actividad Económica Financiera',
+    tipos:['Administración De Propiedades'
+      ,'Concesiones'
+      ,'Contabilidad'
+      ,'Contrataciones'
+      ,'Finanzas'
+      ,'Patrimonio'
+      ,'Permisos'
+      ,'Rentas'
+      ,'Tesoro Municipal'
+     ]},
 
+  ]
+
+  
+  ngOnInit() {
+   
+  }
+
+  subCat(){
+   this.tipos=this.seleccion.tipos;  
+  }
 }
